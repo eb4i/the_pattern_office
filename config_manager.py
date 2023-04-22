e7984cf8825b3b1543aa34048beed8bcf4f545ef
Ian Meikle <sikorska.meikle@icloud.com>
2023-04-22 03:53:51 +0000

    edit test for set

--- config_manager.py
+++ config_manager.py
@@ -22,6 +22,29 @@ class ConfigManager:
 
             # return yaml.load(f, Loader=yaml.FullLoader)
 
-    def save(self, config):
+    # def save(self, config):
+    #     with open(self.config_file, "w") as f:
+    #         yaml.dump(config, f)
+
+    # Get a value from the config
+    def get(self, key: str, default=None):
+        if self.config is None:
+            self.load()
+        return self.config.get(key, default)
+    # Set a value in the config
+    def set(self, key: str, value):
+        if self.config is None:
+            self.load()
+        self.config[key] = value
+        self.save(self.config)
+    
+    # Save the config or optionally pass in a config to save
+    def save(self, config=None):
+        if config is None:
+            config = self.config
         with open(self.config_file, "w") as f:
-            yaml.dump(config, f)
+            yaml.safe_dump(config, f)
+    
+    # def save(self, config):
+    #     with open(self.config_file, "w") as f:
+    #         yaml.dump(config, f)
--- tests/test_config_manager.py
+++ tests/test_config_manager.py
@@ -30,3 +30,64 @@ def test_load_invalid_yaml(tmpdir):
     conf_manager = ConfigManager(invalid_yaml)
     with pytest.raises(ValueError):
         conf_manager.load()
+
+# Test load valid yaml
+def test_load_valid_yaml(tmpdir):
+    valid_yaml = tmpdir / "valid.yaml"
+    valid_yaml.write_text("{'valid': 'yaml'}", encoding="utf-8")
+
+    conf_manager = ConfigManager(valid_yaml)
+    conf_manager.load()
+    assert conf_manager.config == {"valid": "yaml"}
+
+# Test get
+def test_get(tmpdir):
+    valid_yaml = tmpdir / "valid.yaml"
+    valid_yaml.write_text("{'valid': 'yaml'}", encoding="utf-8")
+
+    conf_manager = ConfigManager(valid_yaml)
+    conf_manager.load()
+    assert conf_manager.get("valid") == "yaml"
+    assert conf_manager.get("invalid") is None
+    assert conf_manager.get("invalid", "default") == "default"
+
+# Test set
+def test_set(tmpdir):
+    valid_yaml = tmpdir / "valid.yaml"
+    valid_yaml.write_text("{'valid': 'yaml'}", encoding="utf-8")
+
+    conf_manager = ConfigManager(valid_yaml)
+    conf_manager.load()
+    conf_manager.set("new", "value")
+    assert conf_manager.get("new") == "value"
+#TODO: Test save
+# def test_set_and_save(tmpdir):
+#     test_yaml = tmpdir / "test.yaml"
+
+#     config_manager = ConfigManager(test_yaml)
+#     config_manager.set("key", "value")
+#     # config_manager.save()
+
+#     assert test_yaml.read_text() == "key: value\n"
+
+# Test set and save
+# def test_set(tmpdir):
+#     valid_yaml = tmpdir / "valid.yaml"
+#     valid_yaml.write_text("{'valid': 'yaml'}", encoding="utf-8")
+
+#     conf_manager = ConfigManager(valid_yaml)
+#     conf_manager.load()
+#     conf_manager.set("new", "value")
+#     conf_manager.save()
+#     assert valid_yaml.read_text(encoding="utf-8") == "{'new': 'value\n'}"
+
+# def test_save(tmpdir):
+#     valid_yaml = tmpdir / "valid.yaml"
+#     valid_yaml.write_text("{'valid': 'yaml'}", encoding="utf-8")
+
+#     conf_manager = ConfigManager(valid_yaml)
+#     conf_manager.load()
+#     conf_manager.set("new", "value")
+#     conf_manager.save(valid_yaml)
+#     assert valid_yaml.read_text(encoding="utf-8") == "{'new': 'value'}"
+
